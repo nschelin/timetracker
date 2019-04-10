2dcd8f0b83e0a2646333c3bdbf91d1e1cf7dd698
Nate S <n.schelin@hotmail.com>
2019-04-09 17:19:06 -0700

    updated to use angular cli; not fully tested

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-ng';
+  title = 'Time Tracker';
 }
 
 // import { Component } from '@angular/core';
