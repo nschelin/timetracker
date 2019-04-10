aa51745ebdf9830195280c2215e18fd11aba8000
Nate S <n.schelin@hotmail.com>
2019-04-09 17:06:29 -0700

    further updates to angular cli

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'test-ng';
+}
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app',
+//   templateUrl: './app/app.component.html' 
+// })
+
+// export class AppComponent {
+ 
+// }
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { RouterModule, Routes, Router } from '@angular/router';
+// import { LocationStrategy, HashLocationStrategy  } from '@angular/common';
+// import { AppComponent }  from './app.component';
+// // import { SearchComponent } from './search/search';
+// // import { ResultsListComponent } from './results/results-list';
+// import { NavigationComponent } from './components/navigation/nav.component';
+// import { HomeComponent } from './components/home/home.component';
+// import { ProjectCodeListComponent } from './components/projectcodes/projectcode.list.component';
+// // import { AboutComponent } from './views/about/about';
+
+// const routes: Routes = [
+//   { path: '', component: HomeComponent },
+//   { path: 'projectcodes', component: ProjectCodeListComponent }
+// ];
+
+// @NgModule({
+//   imports: [ BrowserModule , RouterModule.forRoot(routes) ],
+//   declarations: [ 
+//                   AppComponent,
+//                   // SearchComponent,
+//                   // ResultsListComponent,
+//                   NavigationComponent,
+//                   HomeComponent,
+//                   ProjectCodeListComponent
+//                   // AboutComponent
+//                 ],
+//   providers:[{provide: LocationStrategy, useClass: HashLocationStrategy}],
+//   bootstrap: [ AppComponent ]
+// })
+
+// export class AppModule { }
