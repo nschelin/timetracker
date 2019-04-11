fc3dec678b67c5852f698e261d7be01ec86d0f3a
Nathan Schelin <n.schelin@hotmail.com>
2019-04-10 23:43:08 -0700

    work on nedb async

--- server/src/api/controllers/clientsController.js
+++ server/src/api/controllers/clientsController.js
@@ -1,59 +1,34 @@
 const db = require('../../db');
 
-// const clientFind = util.promisify(db.clients['find']);
-// console.log(db.clients);
-
 exports.list = async (req, res) => {
-	// try {
-	// 	const clients = await clientFind();
-	// 	res.send(clients);
-	// } catch (e) {
-	// 	res.send({ message: 'error', error: e });
-	// }
-
 	const clients = await db.clients.findAsync({});
 	res.send(clients);
-	// db.clients.find({}, (err, clients) => {
-	// 	if (err) throw err;
-
-	// 	res.send(clients);
-	// });
 };
 
 exports.add = async (req, res) => {
 	const client = req.body;
-	const foundClinet = await db.clients.findOneAsync({ name: client.name });
-	if (!foundClinet) {
+	const foundClient = await db.clients.findOneAsync({ name: client.name });
+	if (!foundClient) {
 		const newClient = await db.clients.insertAsync(client);
 		res.send(newClient);
 	} else {
-		res.status(400).send('Client Already Exists');
+		res.sendStatus(400).send('Client Already Exists');
 	}
-
-	// db.clients.findOne({ name: client.name }, (err, foundClient) => {
-	// 	if (err) throw err;
-
-	// 	if (!foundClient) {
-	// 		db.clients.insert(client, (err, newClient) => {
-	// 			res.send(newClient);
-	// 		});
-	// 	} else {
-	// 		res.status(400).send('Client Already Exists');
-	// 	}
-	// });
 };
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
 	const id = req.params.id;
 	const client = req.body;
 
-	db.clients.update(
-		{ _id: id },
-		client,
-		{ returnUpdatedDocs: true, multi: false },
-		function(err, numAffected, updateClient) {
-			if (err) throw err;
-			res.send(updateClient);
-		}
-	);
+	try {
+		const updatedClient = await db.clients.updateAsync({ _id: id }, client, {
+			returnUpdatedDocs: true,
+			multi: false
+		});
+		console.log(`updatedClient: ${updatedClient}`);
+		res.send(updatedClient);
+	} catch (e) {
+		console.log(e);
+		res.sendStatus(500).send(e);
+	}
 };
--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,8 +1,41 @@
-const util = require('util');
+//const { promisify } = require('util');
+// //const { promisify } = require('util');
+const Promise = require('bluebird');
 const DataStore = require('nedb');
-DataStore.prototype.findAsync = util.promisify(DataStore.prototype.find);
-DataStore.prototype.findOneAsync = util.promisify(DataStore.prototype.findOne);
-DataStore.prototype.insertAsync = util.promisify(DataStore.prototype.insert);
+
+DataStore.prototype.findAsync = Promise.promisify(DataStore.prototype.find);
+DataStore.prototype.findOneAsync = Promise.promisify(
+	DataStore.prototype.findOne
+);
+DataStore.prototype.insertAsync = Promise.promisify(DataStore.prototype.insert);
+DataStore.prototype.updateAsync = Promise.promisify(
+	DataStore.prototype.update,
+	{ multiArgs: true }
+);
+
+// DataStore.prototype.findAsync = promisify(DataStore.prototype.find);
+// DataStore.prototype.findOneAsync = promisify(DataStore.prototype.findOne);
+// DataStore.prototype.insertAsync = promisify(DataStore.prototype.insert);
+// DataStore.prototype.updateAsync = promisify(
+// 	(query, update, options) =>
+// 		new Promise((resolve, reject) => {
+// 			DataStore.prototype._update(
+// 				query,
+// 				update,
+// 				options,
+// 				(err, numAffected, docs, upsert) => {
+// 					console.log('update callback');
+// 					if (err) {
+// 						return reject(err);
+// 					} else {
+// 						return resolve({ numAffected, docs, upsert });
+// 					}
+// 				}
+// 			);
+// 		})
+// );
+
+//DataStore.prototype.updateAsync = promisify(DataStore.prototype.update);
 //const db = new DataStore({ filename: 'timetracker.db', autoload: true })
 const db = {};
 db.clients = new DataStore({ filename: 'clients.db', autoload: true });
