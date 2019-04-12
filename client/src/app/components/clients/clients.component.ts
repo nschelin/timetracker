7d5bad98d460008420530f3c2a186f90f92bb24b
Nathan Schelin <n.schelin@hotmail.com>
2019-04-12 00:03:00 -0700

    updates to client add

--- client/src/app/components/clients/clients.component.ts
+++ client/src/app/components/clients/clients.component.ts
@@ -7,18 +7,22 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./clients.component.scss']
 })
 export class ClientsComponent implements OnInit {
+  public originalClient: any;
 	public clients: any[];
 	public editIndex: Number = -1;
 
 	constructor(private clientService: ClientService) {}
 
 	addClient() {
-    const newClient = { name: ''};
-    this.clients.unshift(newClient);
-    this.editIndex = 0;
-  }
-	editClient(index: Number) {
-		this.editIndex = index;
+		const newClient = { name: '' };
+		this.clients.unshift(newClient);
+		this.editIndex = 0;
+	}
+
+	editClient(index: Number, client) {
+    console.log(client);
+    this.editIndex = index;
+    this.originalClient = client;
 	}
 
 	saveClient(client) {
@@ -26,6 +30,17 @@ export class ClientsComponent implements OnInit {
 			this.editIndex = -1;
 		});
 	}
+
+	cancelItem(index, client) {
+    this.editIndex = -1;
+    if(typeof client._id === 'undefined') {
+      this.clients.shift();
+    }
+    else if(this.originalClient !== null) {
+      this.clients[index] = this.originalClient;
+      this.originalClient = null;
+    }
+  }
 
 	ngOnInit() {
 		this.clientService
