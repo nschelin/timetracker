59d3ea7acc02485c3851e77c7f3b76fb7b733e46
Nathan Schelin <n.schelin@hotmail.com>
2019-08-21 23:40:52 -0700

    added date package; work on generating weeks

--- client/src/app/components/timecards/timecards.component.ts
+++ client/src/app/components/timecards/timecards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getWeek } from 'date-fns';
 
 import { TimeCard } from '../../models/timecard';
 import { TimeCardWeek } from 'src/app/models/timecardWeek';
@@ -10,14 +11,30 @@ import { TimeCardWeek } from 'src/app/models/timecardWeek';
 })
 export class TimecardsComponent implements OnInit {
 	public timecards: TimeCard[];
-	public currentTimeCard: TimeCard;
+	public recentWeekNumbers: number[] = [];
+	public currentWeek: number;
 	public show = false;
 
 	constructor() {}
+
+	generateWeeks() {
+		const currentWeek = getWeek(Date.now());
+		for (let i = currentWeek + 5; i >= currentWeek - 5; i--) {
+			this.recentWeekNumbers.push(i);
+		}
+	}
+
+	setCurrentWeek(weekNum: number = -1) {
+		this.currentWeek = +weekNum === -1 ? getWeek(Date.now()) : weekNum;
+		// create week
+		this.show = false;
+	}
 
 	addTimeCard() {
 		this.show = true;
 	}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.generateWeeks();
+	}
 }
